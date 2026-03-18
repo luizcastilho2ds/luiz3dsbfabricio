94a380a38d46c1aa5228c77c8ed43522d6a97885
luizcastilho2ds <luizota2009@gmail.com>
2026-03-18 12:15:55 -0300

    Add files via upload

---
+++ luiz/index.js
@@ -0,0 +1,5 @@
+let n1 = Number(prompt("Informe o primeiro número"))
+let n2 = Number(prompt("Informe o segundo número"))
+let soma = n1 + n2
+alert(soma)
+document.writeln(`A soma do numero${n1} com o numero ${n2}`)
